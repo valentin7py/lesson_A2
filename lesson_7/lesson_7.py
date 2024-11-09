b65fcb4e5cabaf89ba3a79cf26a0c4c80abccf4f
valentin7py <mateiv1997@gmail.com>
2024-11-09 09:06:04 +0300

    add module exception and check list,int(age)

--- lesson_7/lesson_7.py
+++ lesson_7/lesson_7.py
@@ -15,6 +15,7 @@
 Valentin: не понял 4 задание."""
 
 import random
+from exceptions import ValidateError
 
 
 class Data:
@@ -24,12 +25,18 @@ class Data:
         self.age = age
         self.name = name
         self.clear_whitespaces()
+        self._validate_age_num()
         self.age = int(age)
 
-    def clear_whitespaces(self):
+    def clear_whitespaces(self) -> None:
         """Удаляет пробелы"""
         self.age = self.age.strip()
         self.name = self.name.strip()
+
+    def _validate_age_num(self) -> None:
+        """Проверка age на числовое значение"""
+        if self.age.isdigit() == False:
+            raise ValidateError('возраст должен быть числом')
 
 
 class Validate:
@@ -40,43 +47,47 @@ class Validate:
 
     def validate(self, data: Data):
         """Добавляем и проверяем данные"""
-        self.data_history.append(
-            data.name)  # добавляем имя в список data_histiry
-        # добавляем возраст в список data_histiry
-        self.data_history.append(data.age)
+        self.data_history.append(data.name)  # добавляем имя в список data_histiry
+        self.data_history.append(data.age) # добавляем возраст в список data_histiry
+        self._validate_chek_data_history() # проверяем список на пустоту
         self.validate_name()  # проверка имени
         self.validate_age()  # проверка возраста
 
-    def validate_name(self)->None:
+    def _validate_chek_data_history(self) -> None:
+        """Проверяем пустой ли список data_history"""
+        if not self.data_history:
+            raise ValueError('Спсиок пуст, сначала заполните его')
+
+    def validate_name(self) -> None:
         """Проверка имени"""
         name = self.data_history[0]
         if name == False:
-            raise Exception('Ошибка: пустое значиние')
+            raise ValidateError('пустое значиние')
         elif name.count(' ') > 1:
-            raise Exception(
-                "Ошибка: В имени между буквами допускается только 1 пробел.")
+            raise ValidateError(
+                "В имени между буквами допускается только 1 пробел.")
         elif name.replace(' ', '').isalpha() == False:
-            raise Exception('Ошибка: должны быть только буквы')
+            raise ValidateError('должны быть только буквы')
         elif len(name) < 3:
-            raise Exception(
-                'Ошибка: В имени должно быть минимум 3 символа.')
+            raise ValidateError(
+                'В имени должно быть минимум 3 символа.')
 
     def validate_age(self) -> None:
         """Проверка возраста"""
         age = self.data_history[-1]
         if age <= 0:
-            raise Exception(
-                'Ошибка: возраст не можеть быть равен или меньше 0')
+            raise ValidateError(
+                'возраст не можеть быть равен или меньше 0')
         elif age < 14:
-            raise Exception('Ошибка: Минимальный возраст — 14 лет')
+            raise ValidateError('Минимальный возраст — 14 лет')
         elif age > 111:
-            raise Exception('Ты кто?')
+            raise ValidateError('Ты кто?')
 
-    def get_passport_advise(self)-> None:
+    def get_passport_advise(self) -> None:
         """Проверяет возраст и записывает совет если надо"""
         age = self.data_history[-1]
         name = self.data_history[0]
-        self.result_text = '' # Выводим строку если не пустая 
+        self.result_text = ''  # Выводим строку если не пустая
         default_text = f'Привет,{name.title()}! Тебе {age} лет.'
         if 16 <= age <= 17:
             self.result_text = default_text + 'Нужно не забыть получить первый паспорт'
@@ -89,8 +100,8 @@ class Validate:
 def guess_number_game() -> None:
     """Игра угадай число"""
     print('Давай поиграем!')
-    check = 0 # флаг для цикла
-    count = 0 # кол-во попыток угадать число
+    check = 0  # флаг для цикла
+    count = 0  # кол-во попыток угадать число
     rand_num = random.randrange(1, 6)
     while check == 0:
         count += 1
@@ -103,17 +114,20 @@ def guess_number_game() -> None:
 
 def main() -> None:
     """Основная функция"""
-    count = 0 # кол-во попыток ввода данных
+    count = 0  # кол-во попыток ввода данных
     while True:
         count += 1
         print(f'Попытка номер: {count}')
         name = input('Введите имя: ')
         age = input('Введите возраст: ')
         try:
-            person = Data(name, age) # 1.создаем обьект класса 'person' с данными 'name' и 'age'
-            valid = Validate() #  2.создаем обьект 'valid' 
-            valid.validate(person)  # 3.проверяем данные (2 и 3 пункт наверно надо обьеденить) 
-        except Exception as e:
+            # 1.создаем обьект класса 'person' с данными 'name' и 'age'
+            person = Data(name, age)
+            # 2.создаем обьект 'valid'
+            valid = Validate() 
+            # 3.проверяем данные (2 и 3 пункт наверно надо обьеденить)
+            valid.validate(person)
+        except ValidateError as e:
             print(f'я поймал ошибку: - {e}')
         else:
             break
@@ -122,10 +136,8 @@ def main() -> None:
     if valid.result_text:  # печатаем приветствие и совет если надо.
         print(valid.result_text)
 
-    guess_number_game() # запускаем игру угадай число
+    guess_number_game()  # запускаем игру угадай число
 
 
 if __name__ == "__main__":
     main()
-
-#wewq
