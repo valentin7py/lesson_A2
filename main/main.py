67f239ca17c444bee87b8dfec2c6fce72f5696be
valentin7py <mateiv1997@gmail.com>
2024-11-12 14:17:04 +0300

    func main wrapped in decorator

--- main/main.py
+++ main/main.py
@@ -1,5 +1,7 @@
 """
-lesson 9
+Написать декоратор для функции main, в котором будет в бесконечном цикле вызов main до тех пор,
+пока main функция не вернет значение True. True значение должно вернуться в том случае,
+если в main не было ни одной ошибки авторизации и пользователь был успешно авторизовано/зарегистрирован
 """
 from authenticator import Authenticator
 from exceptions import RegistrationError,AuthorizationError
@@ -19,32 +21,43 @@ def guess_number_game() -> None:
             check += 1
             return print('Ура ты угадал!!!')
 
+def decor(func):
+    def wraper():
+        while True:
+            if func() == True:
+                break
+            
+    return wraper
 
-def main() -> None:
+@decor        
+def main() -> bool:
     """Основная функция"""
     authenticator = Authenticator()
+
+    login = input('Введите логин: ')
+    password = input('Введите пароль: ')
     
-    while True:
-        login = input('Введите логин: ')
-        password = input('Введите пароль: ')
-        
-        if authenticator.login:     
-            try:
-                authenticator.authorize(login,password) # авторизация
-                authenticator.user_greeting() # финальное приветсвие
-                break
-            except AuthorizationError as e:
-                print(e)
-        else:
+    if authenticator.login:     
+        try:
+            authenticator.authorize(login,password) # авторизация
+            authenticator.user_greeting() # финальное приветсвие
+            guess_number_game()
+            return True
             
-            try:
-                authenticator.registrate(login,password)# регистрируем пользователя
-            except RegistrationError as e:
-                print(e)
-            break
+        except AuthorizationError as e:
+            print(e)
+    else:
+        
+        try:
+            authenticator.registrate(login,password)# регистрируем пользователя
+            guess_number_game()
+            return True
+            
+        except RegistrationError as e:
+            print(e)
 
-    guess_number_game()  # запускаем игру угадай число
-
+    
 
 if __name__ == "__main__":
     main()
+
