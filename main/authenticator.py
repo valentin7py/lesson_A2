30f9f3d4bb2f53e2a0b391553fa7b65f169a379d
valentin7py <mateiv1997@gmail.com>
2024-11-13 13:15:12 +0300

    lesson 11. write files json

--- main/authenticator.py
+++ main/authenticator.py
@@ -1,7 +1,14 @@
+"""lesson 11
+
+1. Доделать последнюю домашку
+
+2. В файл сохранять теперь данные в формате json
+
+3. Разобраться как сериализовать datetime в json (Гугл, а потом написать подробно в комменте почему именно так)"""
 import os
 from exceptions import RegistrationError, AuthorizationError
 import datetime
-
+import json
 
 class Authenticator:
     def __init__(self):
@@ -10,26 +17,28 @@ class Authenticator:
         self.last_success_login_at: datetime | None = None
         self.errors_count: int = 0  # количество проваленных попыток (ошибки)
 
-        if self._is_auth_file_exist(): # проверяем есть создан ли файл
-            self._read_auth_file() # записываем данные из файла в переменные об.класса
+        if self._is_auth_file_exist():  # проверяем есть создан ли файл
+            self._read_auth_file()  # записываем данные из файла в переменные об.класса
         else:
-            print('Регистрация') 
+            print('Регистрация')
 
     def _is_auth_file_exist(self) -> bool:
         """Проверяем наличие файла `auth.txt` рядом (в той же папке)"""
-        is_file_exist = "/home/valenit/Документы/studies/lesson_a/lessons_A/main/auth.txt" 
+        is_file_exist = "/home/valenit/Документы/studies/lesson_a/lessons_A/main/auth.json"
         #  почему то рабоатет только с полным путем.
         if os.path.isfile(is_file_exist):
             return True
         return False
 
-    def _read_auth_file(self):
+    def _read_auth_file(self) -> None:
         """Данные из файла записываем в переменные"""
-        with open("main/auth.txt", 'r') as f:
-            self.login = f.readline().strip()
-            self._password = f.readline().strip()
-            self.last_success_login_at = f.readline().strip()
-            self.errors_count = int(f.readline().strip())
+        f = open('main/auth.json')
+        dict_data = json.load(f)
+        self.login = dict_data['login']
+        self._password = dict_data['password']
+        self.last_success_login_at = dict_data['last_success_login_at']
+        self.errors_count = dict_data['errors_count']
+        
 
     def authorize(self, login: str, password: str) -> None:
         """1.Проеврка сущесвует ли  login.
@@ -44,15 +53,15 @@ class Authenticator:
         else:
             self.errors_count += 1
             raise AuthorizationError('Пустой логин')
-            
 
-    def registrate(self, login: str, password: str):
+    def registrate(self, login: str, password: str)-> None:
         """Проверяем зарегистрирован ли пользователь"""
         if self.login:  # проверка наличия файла в папке
             self.errors_count += 1
-            raise RegistrationError("Пользвоатель с таким именем уже существует")
+            raise RegistrationError(
+                "Пользвоатель с таким именем уже существует")
 
-        if login: 
+        if login:
             self.login = login
             self._password = password
             self._update_auth_file()
@@ -61,27 +70,16 @@ class Authenticator:
             self._update_auth_file
             raise RegistrationError("Ошибка: пустой логин")
 
-    def _update_auth_file(self):
-        """Перезаписывает данные в файл"""
-        utc_now = datetime.datetime.utcnow()
-        utc_date_time = utc_now.strftime("%m/%d/%Y, %H:%M:%S")
-        self.last_success_login_at = utc_date_time
-        with open('main/auth.txt', 'w') as f:
-            f.write(f'{self.login}\n')
-            f.write(f'{self._password}\n')
-            f.write(f'{self.last_success_login_at}\n')
-            f.write(f'{self.errors_count}')
-    
-    
-    def user_greeting(self):
+    def _update_auth_file(self)-> None:
+        """Перезаписывает данные в json файл"""
+        utc_now = datetime.datetime.utcnow() # получаем время 
+        utc_date_time = utc_now.strftime("%Y-%m-%d %H:%M:%S") # переводим в удобный формат
+        data = {'login': self.login, 'password': self._password,
+                'last_success_login_at':utc_date_time , 'errors_count': self.errors_count}
+        with open('main/auth.json','w') as file:
+            json.dump(data,file)
+            
+    def user_greeting(self) -> None:
         """Финальное привествие вынес в отдельную функцию"""
-        print(f'Привет {self.login}\nВремя входа - {self.last_success_login_at}\nКол-во пыпоток входа - {self.errors_count}')
-          
-        
-           
-
-   
-    
-
-
-
+        print(
+            f'Привет {self.login}\nВремя входа - {self.last_success_login_at}\nКол-во пыпоток входа - {self.errors_count}')
